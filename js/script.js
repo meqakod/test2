679740d64fc0ba09b3d63cf9c98961386c150ada
Rashad <meqakod@gmail.com>
2022-08-31 11:38:24 +0400

    promt

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 "use sctrict";
-const answer = prompt("Are you turk?")
-console.log(answer)
+const numberOfFilms = prompt("How many movies you watched ?");
+const personalMovieBD = {
+    "count": numberOfFilms,
+    "movies": {},
+    "actors": {},
+    "genres": [],
+    "ptivat": false
+};
+const lastMovie = prompt("Last watched movie");
+const lastMovieRaiting = prompt("Last watched movie raiting");
+personalMovieBD.movies[lastMovie] = lastMovieRaiting;
+console.log(personalMovieBD)
